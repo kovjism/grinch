20d6ed5958fd00c5ea8cf01456b4f8a78fe13ce9
aaronchang1 <aaron489chang@gmail.com>
2025-04-11 18:06:45 -0500

    sound overlap fix

--- Assets/Scripts/Managers/SoundFXManager.cs
+++ Assets/Scripts/Managers/SoundFXManager.cs
@@ -1,10 +1,13 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SoundFXManager : MonoBehaviour
 {
     public static SoundFXManager instance;
-
+    private float minSoundInterval = 1.0f;
     [SerializeField] private AudioSource soundFXObject;
+    private AudioSource[] audioSources = new AudioSource[32];
     private void Awake()
     {
         if (instance == null)
@@ -15,11 +18,33 @@ public class SoundFXManager : MonoBehaviour
 
     public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
     {
+        foreach (AudioSource source in audioSources)
+        {
+            if (source != null)
+            if (source.isPlaying && source.clip.name == audioClip.name && source.time <= minSoundInterval)
+                return;
+        }
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
         audioSource.clip = audioClip;
         audioSource.volume = volume;
         audioSource.Play();
+        for (int i = 0; i < 32; i++)
+        {
+            if (audioSources[i] == null)
+            {
+                audioSources[i] = audioSource;
+                break;
+            }
+        }
         float clipLength = audioSource.clip.length;
         Destroy(audioSource.gameObject, clipLength);
+        for (int i = 0; i < 32; i++)
+        {
+            if (audioSources[i] == audioClip)
+            {
+                audioSources[i] = null;
+                break;
+            }
+        }
     }
 }
