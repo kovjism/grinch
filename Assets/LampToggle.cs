59b5cd50c4d2a88d78839c60f79e6a5e32fb7b57
Sankalp Shubham <87834473+sankalpshubham@users.noreply.github.com>
2025-04-11 18:05:47 -0500

    lamp feature

---
+++ Assets/LampToggle.cs
@@ -0,0 +1,36 @@
+//using UnityEngine;
+
+//public class LampToggle : MonoBehaviour
+//{
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+//}
+
+using UnityEngine;
+
+public class LampToggle : MonoBehaviour
+{
+    public Light lampLight; // Assign this in the Inspector
+
+    private bool isOn = false;
+
+    void Start()
+    {
+        lampLight.enabled = isOn;
+    }
+
+    public void ToggleLamp()
+    {
+        isOn = !isOn;
+        lampLight.enabled = isOn;
+    }
+}
