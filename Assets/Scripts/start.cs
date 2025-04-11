18a202d1afb956350622d959cbcb2f256b1fec55
Alex Wang <awang2004j@gmail.com>
2025-04-10 23:20:28 -0500

    walls and floor

---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,24 @@
+using Palmmedia.ReportGenerator.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("grinch");
+    }
+
+    public void Options()
+    {
+        Canvas settings = GameObject.Find("Menu").GetComponent<Canvas>();
+        settings.enabled = false;
+        Canvas options = GameObject.Find("Options").GetComponent<Canvas>();
+        options.enabled = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
