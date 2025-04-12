8b623a9a2a41b3cfe900da5c31b70404c20f2c29
Sankalp Shubham <87834473+sankalpshubham@users.noreply.github.com>
2025-04-11 20:08:21 -0500

    diverse interactable items complete

--- Assets/Scripts/shoot.cs
+++ Assets/Scripts/shoot.cs
@@ -47,4 +47,55 @@ public class shoot : MonoBehaviour
             guns[i].gameObject.SetActive(i == currentGun);
         }
     }
+
+    // Add this method to your shoot class
+    public bool AddGun(GameObject gunPrefab)
+    {
+        if (gunPrefab == null) return false;
+
+        // Check if this gun is already in our inventory
+        gun gunComponent = gunPrefab.GetComponent<gun>();
+        if (gunComponent == null)
+        {
+            Debug.LogError("Gun prefab doesn't have a gun component!");
+            return false;
+        }
+
+        // Check if we already have this gun type
+        foreach (gun existingGun in guns)
+        {
+            if (existingGun.GetType() == gunComponent.GetType())
+            {
+                Debug.Log("You already have this gun type!");
+                return false;
+            }
+        }
+
+        // Instantiate the gun as a child of this object
+        GameObject newGun = Instantiate(gunPrefab, transform);
+
+        // Reset transform to make sure it's in view
+        newGun.transform.localPosition = new Vector3(0.4f, -0.2f, 1f);  // later change this to the EXACT position
+        newGun.transform.localRotation = Quaternion.identity;
+
+        gun newGunComponent = newGun.GetComponent<gun>();
+
+        // Setup camera reference for the new gun
+        newGunComponent.cameraObject = guns[0].cameraObject;
+
+        // Add to our guns list
+        guns.Add(newGunComponent);
+
+        // Initially disable the gun since it's not currently selected
+        newGun.SetActive(false);
+
+        // If this is our first gun, select it
+        if (guns.Count == 1)
+        {
+            currentGun = 0;
+            UpdateWeaponVisibility();
+        }
+
+        return true;
+    }
 }
