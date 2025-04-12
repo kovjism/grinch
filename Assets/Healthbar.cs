fa0e35e361ab895e3612b345e3103425702c955a
Alex Wang <awang2004j@gmail.com>
2025-04-12 00:11:47 -0500

    Revert "health bar update and scene update"
    This reverts commit 1b0a601708901e542d35db3e0c7ae37ae1a138ae.
    
    merge conflict with jichuan

--- Assets/Healthbar.cs
+++ Assets/Healthbar.cs
@@ -4,14 +4,7 @@ using UnityEngine.UI;
 public class Healthbar : MonoBehaviour
 {
     [SerializeField] private Image healthbarSprite;
-    [SerializeField] private float reduceSpeed = 3;
-    private float target = 1;
-
     public void UpdateHealthBar(float maxHealth, float currentHealth) {
-        target = currentHealth / maxHealth;
-    }
-
-    void Update() {
-        healthbarSprite.fillAmount = Mathf.MoveTowards(healthbarSprite.fillAmount, target, reduceSpeed * Time.deltaTime);
+        healthbarSprite.fillAmount = currentHealth / maxHealth;
     }
 }
