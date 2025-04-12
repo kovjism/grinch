cfdf79268cbfd264f1dfde0551a57c7f02ec8943
Alex Wang <awang2004j@gmail.com>
2025-04-12 18:15:30 -0500

    present opening sound

--- Assets/Scripts/PresentOpener.cs
+++ Assets/Scripts/PresentOpener.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 public class PresentOpener : MonoBehaviour
 {
     public GameObject gunPickupPrefab; 
-
+    [SerializeField] private AudioClip giftSoundClip;
     private bool isOpened = false;
 
     public void Open()
     {
+        SoundFXManager.instance.PlaySoundFXClip(giftSoundClip, transform, 0.2f);
+
         if (isOpened) return;
 
         isOpened = true;
-
         // Spawn the gun at the present's position
         Instantiate(gunPickupPrefab, transform.position, transform.rotation);
 
